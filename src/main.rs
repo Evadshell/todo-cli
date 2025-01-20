2387cf621fb81d1d71c40da95c4a595c8f5104df
evadshell <evadshell69@gmail.com>
2025-01-20 17:43:23 +0530

    basic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
-#![allow(dead_code,unused)]
-use termion::{raw::IntoRawMode, terminal_size};
+#![allow(dead_code, unused)]
+use termion::{event::Key, input::TermRead, raw::IntoRawMode, terminal_size};
 
-use std::io::{Write, stdout};
+use std::{
+    fmt::write,
+    io::{Write, stdin, stdout},
+};
 
 fn main() {
-  
- 
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(
+        stdout,
+        "{}{}q to exit , Type stuff and all. {}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide,
+    )
+    .unwrap();
+    stdout.flush().unwrap();
+    for k in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::clear::CurrentLine
+        )
+        .unwrap();
+        match k.as_ref().unwrap() {
+            Key::Char('q') => break,
+            _ => println!("{:?}", k),
+        }
+        stdout.flush().unwrap();
+    }
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
 }
