c3c35c10c032ae949f70a0f9f784a50b32de4507
evadshell <evadshell69@gmail.com>
2025-01-20 19:04:47 +0530

    change from termion to ncurses

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,23 @@
-#![allow(dead_code, unused)]
-use termion::{event::Key, input::TermRead, raw::IntoRawMode, terminal_size};
-
-use std::{
-    fmt::write,
-    io::{Write, stdin, stdout},
-};
-
+extern crate ncurses;
+use ncurses::*;
 fn main() {
-    let stdin = stdin();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    write!(
-        stdout,
-        "{}{}q to exit , Type stuff and all. {}",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1),
-        termion::cursor::Hide,
-    )
-    .unwrap();
-    stdout.flush().unwrap();
-    for k in stdin.keys() {
-        write!(
-            stdout,
-            "{}{}",
-            termion::cursor::Goto(1, 1),
-            termion::clear::CurrentLine
-        )
-        .unwrap();
-        match k.as_ref().unwrap() {
-            Key::Char('q') => break,
-            _ => println!("{:?}", k.unwrap()),
+    initscr();
+    let mut todos = vec!["make a todo", "learn rust"];
+    let mut quit = false;
+    while !quit {
+        // addstr("test");
+        for (row, todo) in todos.iter().enumerate() {
+            mv(row as i32, 1);
+            addstr(*todo);
         }
-        stdout.flush().unwrap();
+        let key = getch();
+        refresh();
+        match key as u8 as char {
+            'q' => quit = true,
+            // _ => {addstr(key as &str).unwrap()}
+            _ => {}
+        }
     }
-    write!(stdout, "{}", termion::cursor::Show).unwrap();
+    getch();
+    endwin();
 }
