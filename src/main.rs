aa8bd7235dfd277e6777767a50d5a37e976b7804
evadshell <evadshell69@gmail.com>
2025-01-20 17:29:20 +0530

    mkc

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-extern crate termion;
+use termion::{raw::IntoRawMode, terminal_size};
 
-use termion::event::{Key, Event, MouseEvent};
-use termion::input::{TermRead, MouseTerminal};
-use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
+use std::io::{Write, stdout};
 
 fn main() {
-    let stdin = stdin();
-    let mut stdout = MouseTerminal::from(stdout().into_raw_mode().unwrap());
+  
+    println!("{:?}", terminal_size());
+  
+    // println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+    //     let mut stdout = stdout().into_raw_mode()?;
+    //   write!(stdout, "Hey there .")?;
 
-    write!(stdout, "{}{}q to exit. Click, click, click!", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
-    stdout.flush().unwrap();
-
-    for c in stdin.events() {
-        let evt = c.unwrap();
-        match evt {
-            Event::Key(Key::Char('q')) => break,
-            Event::Mouse(me) => {
-                match me {
-                    MouseEvent::Press(_, x, y) => {
-                        write!(stdout, "{}x", termion::cursor::Goto(x, y)).unwrap();
-                    },
-                    _ => (),
-                }
-            }
-            _ => {}
-        }
-        stdout.flush().unwrap();
-    }
+    //   Ok(())
 }
