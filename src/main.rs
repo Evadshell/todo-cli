3d003a94f732e983a674ed0440a48ae85f784f34
evadshell <evadshell69@gmail.com>
2025-01-20 17:30:07 +0530

    mkc

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
+#![allow(dead_code,unused)]
 use termion::{raw::IntoRawMode, terminal_size};
 
 use std::io::{Write, stdout};
 
 fn main() {
   
-    println!("{:?}", terminal_size());
-  
-    // println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
-    //     let mut stdout = stdout().into_raw_mode()?;
-    //   write!(stdout, "Hey there .")?;
-
-    //   Ok(())
+ 
 }
