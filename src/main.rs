0629abd90846237837a6d607e6aa6ae39f74eac3
evadshell <evadshell69@gmail.com>
2025-01-21 10:57:21 +0530

    self

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 extern crate ncurses;
 use ncurses::*;
 const REGULAR_PAIR: i16 = 0;
@@ -5,6 +6,8 @@ const HIGHLIGHT_PAIR: i16 = 1;
 
 fn main() {
     initscr();
+    noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
     start_color();
     init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
     init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
@@ -12,8 +15,9 @@ fn main() {
     let mut todos = vec!["make a todo", "learn rust", "muheuehueh"];
     let mut quit = false;
     let mut todo_curr: usize = 1;
+    let mut done = Vec::<&str>::new();
     while !quit {
-        // addstr("test");
+        addstr("TODO :");
         for (index, todo) in todos.iter().enumerate() {
             let pair = {
                 if todo_curr == index {
@@ -27,6 +31,11 @@ fn main() {
             addstr(*todo).unwrap();
             attroff(COLOR_PAIR(pair));
         }
+        addstr("DONE");
+        for (index, don) in done.iter().enumerate() {
+            mv(index as i32, 30);
+            addstr(*don);
+        }
         let key = getch();
         refresh();
         match key as u8 as char {
@@ -35,12 +44,20 @@ fn main() {
             'w' => {
                 if todo_curr > 0 {
                     todo_curr = todo_curr - 1
+                } else if todo_curr == 0 {
+                    todo_curr = todos.len() - 1;
                 }
             }
             's' => {
-                if todo_curr < todos.len() {
+                if todo_curr < todos.len() - 1 {
                     todo_curr = todo_curr + 1
+                } else if todo_curr == todos.len() - 1 {
+                    todo_curr = 0;
                 }
+            }
+            'd' => {
+                done.push(todos[todo_curr]);
+                todos.remove(todo_curr);
             }
             _ => {}
         }
